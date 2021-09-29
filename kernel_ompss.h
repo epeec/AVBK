f56a4fe3b38235e3e94f6c113ce4db2cf874975b
Pavanakumar MOHANAMURALY <mpkumar@cerfacs.fr>
2021-09-29 17:03:25 +0200

    Changes to header

--- kernel_ompss.h
+++ kernel_ompss.h
@@ -5,6 +5,5 @@
 void element_volumes_cgroup(int nnodes, int ncolours, int max_group_size,
                             const int *group_offset, const int *cgroup_offset,
                             const int *cgroup, const double *x, const double *y,
-                            const int *triangles, double *vol_e, double *vol_n);
-
+                            const int *triangles,  double *vol_e, double *vol_n);
 #endif
